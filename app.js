29907148a03e160a734b45718aac2c0b22bd5c79
kimberly denis <denisk2@southern.edu>
2025-10-12 03:49:43 +0000

    updating final changes to app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const http = require('http');
+const fs = require('fs');
 const url = require('url');
+const path = require('path');
 
+//appointment array
 const availableTimes = {
     Monday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
     Tuesday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
@@ -8,110 +11,154 @@ const availableTimes = {
     Thursday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
     Friday: ["1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
 };
+
+//current appointments
 const appointments = [
-    {name: "James", day: "Wednesday", time: "3:30" },
-    {name: "Lillie", day: "Friday", time: "1:00" }];
+    { name: "James", day: "Wednesday", time: "3:30" },
+    { name: "Lillie", day: "Friday", time: "1:00" }
+];
 
-let serverObj =  http.createServer(function(req,res){
-	console.log(req.url);
-	let urlObj = url.parse(req.url,true);
-	switch (urlObj.pathname) {
-		case "/schedule":
-			schedule(urlObj.query,res);
-			break;
-		case "/cancel":
-			cancel(urlObj.query,res);
-			break;
-		case "/check":
- 		//added check
+// error() to replace res when its an error
+function error(response, status, textM) {
+    response.writeHead(status, { 'Content-Type': 'text/plain' });
+    response.write(textM);
+    response.end();
+}
 
-			check(urlObj.query,res);
-			break;
-		default:
-			error(res,404,"pathname unknown");
+// message() to replace res
+function message(response, textM) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write(textM);
+    response.end();
+}
 
-	}
-});
+function schedule(qObj, res) {
+	 //check missing data
+    if (!qObj.name || !qObj.day || !qObj.time) {
+        error(res, 400, "Missing Data");
+        return;
+    }
 
-function schedule(qObj,res) {
-	//check missing data
-	if (!qObj.name || !qObj.day || !qObj.time){
-		error(res,400, "Missing Data");
-		return;
-	}
-	
-	if (availableTimes[qObj.day].some(time => time == qObj.time))
-	{
-		//display message if time is avalible
-		message(res,"scheduled");
-		//get index of location to remove it from array
+    if (availableTimes[qObj.day].some(time => time == qObj.time)) {
+         //display message if time is avalible
+		message(res, "Scheduled successfully");
+         //get index of location to remove it from array
 		const tIndex = availableTimes[qObj.day].indexOf(qObj.time);
-		//index will be -1 if not there, check if there
-		if(tIndex !== -1){
-			availableTimes[qObj.day].splice(tIndex,1);
-		}
-		//add to appointments
+        //index will be -1 if not there, check if there
+		if (tIndex !== -1) availableTimes[qObj.day].splice(tIndex, 1);
+        //add to appointments
 		appointments.push(qObj);
-	}
-	else 
-		error(res,400,"Can't schedule");
-
- 
+    } else {
+        error(res, 400, "Can't schedule: time unavailable");
+    }
 }
 
-function cancel(qObj,res) {
-	//check missing data
-	if (!qObj.name || !qObj.day || !qObj.time){
-		error(res,400, "Missing Data");
-		return;
-	}
-	//get array of appointment if it is there 
-	const aIndex = appointments.findIndex(appt => appt.name === qObj.name && appt.day === qObj.day && appt.time === qObj.time);
+function cancel(qObj, res) {
+	 //check missing data
+    if (!qObj.name || !qObj.day || !qObj.time) {
+        error(res, 400, "Missing Data");
+        return;
+    }
+
+    const aIndex = appointments.findIndex(
+        appt => appt.name === qObj.name && appt.day === qObj.day && appt.time === qObj.time
+    );
 	//index is -1 if not there
-	if (aIndex === -1) {
-		error(res, 400, "Appointment not found");
-	}
-	else{
-	//if its there then we remove from appointments and add to avaliabletimes
-		appointments.splice(aIndex,1);
-		availableTimes[qObj.day].push(qObj.time);
-		message(res,"Appointment has been canceled");
-		
-	}
-	//
-		
+    if (aIndex === -1) {
+        error(res, 400, "Appointment not found");
+    } else {
+		//if its there then we remove from appointments and add to avaliabletimes
+        appointments.splice(aIndex, 1);
+        availableTimes[qObj.day].push(qObj.time);
+        message(res, "Appointment canceled");
+    }
 }
-//
-function check(qObj,res){
+
+function check(qObj, res) {
 	//check if data is missing
-	if (!qObj.day || !qObj.time){
-		error(res,400, "Missing Data");
-		return;
-	}
+    if (!qObj.day || !qObj.time) {
+        error(res, 400, "Missing Data");
+        return;
+    }
+	
 	//if its in the array then its avaliable
-	if (availableTimes[qObj.day].some(time => time == qObj.time))
-	{
-		message(res,"Appointment Avaliable");
-	}
-	else
-	{
+    if (availableTimes[qObj.day]?.includes(qObj.time)) {
+        message(res, "Appointment Available");
+    } else {
 		//if its not then its not
-		message(res,"Appointment Not Avaliable");
-	}
-}
-//handles error message
-function error(response,status,message) {
-
-	response.writeHead(status,{'content-type':'text/plain'});
-	response.write(message);
-	response.end();
-}
-//handles other messages
-function message(response,textM) {
-
-	response.writeHead(200,{'content-type':'text/plain'});
-	response.write(textM);
-	response.end();
+        message(res, "Appointment Not Available");
+    }
 }
 
-serverObj.listen(80,function(){console.log("listening on port 80")});
+//using path.ext to get type and return to send
+function getContentType(filePath) {
+    const ext = path.extname(filePath).toLowerCase();
+    switch (ext) {
+        case '.txt': return 'text/plain';
+        case '.html': return 'text/html';
+        case '.css': return 'text/css';
+        case '.js': return 'application/javascript';
+        case '.json': return 'application/json';
+        case '.jpg':
+        case '.jpeg': return 'image/jpeg';
+        case '.gif': return 'image/gif';
+        case '.png': return 'image/png';
+        default: return 'application/octet-stream';
+    }
+}
+
+//sends files based on type
+function sendFile(filePath, res) {
+    fs.readFile(filePath, function (err, data) {
+        if (err) {
+			//error if not found
+            error(res, 404, 'Error 404: resource not found.');
+        } else {
+			//send file if found
+            res.writeHead(200, { 'Content-Type': getContentType(filePath) });
+            res.write(data);
+            res.end();
+        }
+    });
+}
+
+// Main request handler with routing
+function serveStatic(req, res) {
+    const urlObj = url.parse(req.url, true);
+    const pathname = urlObj.pathname;
+
+	//log paths
+    console.log("Request:", pathname);
+
+	//switch for paths
+    switch (pathname) {
+        case "/schedule":
+            schedule(urlObj.query, res);
+            break;
+
+        case "/cancel":
+            cancel(urlObj.query, res);
+            break;
+
+        case "/check":
+            check(urlObj.query, res);
+            break;
+			
+        case "/":
+        case "/index":
+        case "/index.html":
+            sendFile('./public_html/index.html', res);
+            break;
+
+        default:
+            // send files (added join since js not working with html)
+            const fileName = path.join(__dirname, 'public_html', pathname);
+            sendFile(fileName, res);
+            break;
+    }
+}
+
+//Create and start server
+http.createServer(serveStatic).listen(80, function() {
+    console.log("listening on port 80");
+});
